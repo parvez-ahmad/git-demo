433a79c844ddb990a2754a11844abfbede461a82
parvez09ahmad <parveziahmad09@gmail.com>
2021-05-18 06:59:02 +0530

    new branche created

--- src/main/java/com/javatechie/app/git_demo/App.java
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -3,6 +3,7 @@ package com.javatechie.app.git_demo;
 public class App {
 	public void addData(String data) {
 		System.out.println("data " + data);
+		System.out.println("parvez ahmad");
 	}
 
 	public static void main(String[] args) {
