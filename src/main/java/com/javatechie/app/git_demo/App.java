3ee68de4cc8f35025e695b5513967e6c385891c6
parvez09ahmad <parveziahmad09@gmail.com>
2021-05-18 07:34:05 +0530

    test data added

--- src/main/java/com/javatechie/app/git_demo/App.java
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -4,6 +4,7 @@ public class App {
 	public void addData(String data) {
 		System.out.println("data " + data);
 		System.out.println("parvez ahmad");
+		System.out.println("git data");
 	}
 
 	public static void main(String[] args) {
